20f9e815b4b567ba117ff9d09118ca4c725d0072
Serik <serik.shaikamalov@gmail.com>
2024-09-19 13:44:07 +0300

    remove types folder

--- types/hajjtravel-utils.d.ts
+++
@@ -1,9 +0,0 @@
-declare module 'hajjtravel-utils' {
-    // Add the function and type declarations here
-    export function phoneToBackendFormat(phone: string, countryCode?: string): string;
-    export function isFileBigger(file: File): boolean;
-    export function isImage(file: File): boolean;
-    export function objectToPlain(obj: any, parent?: string, result?: any): any;
-    export function isEmailAddress(email: string): boolean;
-    export function convertToNestedJSON(input: object): object;
-}
